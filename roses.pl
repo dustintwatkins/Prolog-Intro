7e9f59de8197e3e15018c5e51152d40f41113a4b
dustintwatkins <dustintwatkins@gmail.com>
2018-02-26 23:02:17 -0700

    Completed

--- roses.pl
+++ roses.pl
@@ -26,74 +26,70 @@ rose(mountain_bloom).
 rose(pink_paradise).
 rose(sweet_dreams).
 
-
 solve :-
-  rose(hugh_rose), rose(ida_rose), rose(jeremy_rose), rose(leroy_rose), rose(stella_rose),
-  all_different([hugh_rose, ida_rose, jeremy_rose, leroy_rose, stella_rose]),
+  rose(Hugh_rose), rose(Ida_rose), rose(Jeremy_rose), rose(Leroy_rose), rose(Stella_rose),
+  all_different([Hugh_rose, Ida_rose, Jeremy_rose, Leroy_rose, Stella_rose]),
 
-  item(hugh_item), item(ida_item), item(jeremy_item), item(leroy_item), item(stella_item),
-  all_different([hugh_item, ida_item, jeremy_item, leroy_item, stella_item]),
+  item(Hugh_item), item(Ida_item), item(Jeremy_item), item(Leroy_item), item(Stella_item),
+  all_different([Hugh_item, Ida_item, Jeremy_item, Leroy_item, Stella_item]),
 
-  event(hugh_event), event(ida_event), event(jeremy_event), event(leroy_event), event(stella_event),
-  all_different([hugh_event, ida_event, jeremy_event, leroy_event, stella_event]),
+  event(Hugh_event), event(Ida_event), event(Jeremy_event), event(Leroy_event), event(Stella_event),
+  all_different([Hugh_event, Ida_event, Jeremy_event, Leroy_event, Stella_event]),
 
-  groupings = [ [hugh, hugh_rose, hugh_item, hugh_event],
-            [ida, ida_rose, ida_item, ida_event],
-            [jeremy, jeremy_rose, jeremy_item, jeremy_event],
-            [leroy, leroy_rose, leroy_item, leroy_event],
-            [stella, stella_rose, stella_item, stella_event] ],
+  Groupings = [ [hugh, Hugh_rose, Hugh_item, Hugh_event],
+            [ida, Ida_rose, Ida_item, Ida_event],
+            [jeremy, Jeremy_rose, Jeremy_item, Jeremy_event],
+            [leroy, Leroy_rose, Leroy_item, Leroy_event],
+            [stella, Stella_rose, Stella_item, Stella_event] ],
 
-%Clue One
-  %jeremy_event = senior_prom
-  %Stella_event != wedding
-  %stella_rose = cottage_beauty
+% Clue One
+  % Jeremy_event = senior_prom
+  % Stella_event != wedding
+  % Stella_rose = cottage_beauty
 
   %member(person, rose, item, event)
 
-  member([hugh, _, _, _], groupings),
-  member([ida, _, _, _], groupings),
-  member([jeremy, _, _, senior_prom], groupings),
-  member([leroy, _, _, _], groupings),
-  member([stella, cottage_beauty, _, _,], groupings),
+  member([jeremy, _, _, senior_prom], Groupings),
+  member([stella, cottage_beauty, _, _], Groupings),
 
   %stella did not purchase for wedding
-  \+ member(stella, _, _, wedding),
+  \+ member([stella, _, _, wedding], Groupings),
 
 %Clue Two
-  %hugh_rose = pink_paradise
-  %hugh_event != charity_auction
-  %hugh_event != wedding
+  %Hugh_rose = pink_paradise
+  %Hugh_event != charity_auction
+  %Hugh_event != wedding
 
-  member([hugh, pink_paradise, _, _], groupings),
-  \+ member([hugh, _, _, charity_auction], groupings),
-  \+ member([hugh, _, _, wedding], groupings),
+  member([hugh, pink_paradise, _, _], Groupings),
+  \+ member([hugh, _, _, charity_auction], Groupings),
+  \+ member([hugh, _, _, wedding], Groupings),
 
 %Clue Three
   %person_event = anniversary && person_item = streamers
   %person_event = wedding && person_item = balloons
 
-  member([_, _, streamers, anniversary], groupings),
-  member([_, _, balloons, wedding], groupings),
+  member([_, _, streamers, anniversary], Groupings),
+  member([_, _, balloons, wedding], Groupings),
 
 %Clue Four
   %person_rose = sweet_dreams && person_item = chocolates
-  %jeremy_rose != mountain_bloom
+  %Jeremy_rose != mountain_bloom
 
-  member([_, sweet_dreams, chocolates, _], groupings),
-  \+ member([jeremy, mountain_bloom, _, _], groupings),
+  member([_, sweet_dreams, chocolates, _], Groupings),
+  \+ member([jeremy, mountain_bloom, _, _], Groupings),
 
 %Clue Five
-  %leroy_event = retirement
+  %Leroy_event = retirement
   %person_event = senior_prom && person_item = candles
 
-  member([leroy, _, _, retirement], groupings),
-  member([_, _, candles, senior_prom], groupings),
+  member([leroy, _, _, retirement], Groupings),
+  member([_, _, candles, senior_prom], Groupings),
 
-  tell(hugh, hugh_rose, hugh_item, hugh_event),
-  tell(ida, ida_rose, ida_item, ida_event),
-  tell(jeremy, jeremy_rose, jeremy_item, jeremy_event),
-  tell(leroy, leroy_rose, leroy_item, leroy_event),
-  tell(stella, stella_rose, stella_item, stella_event).
+  tell(hugh, Hugh_rose, Hugh_item, Hugh_event),
+  tell(ida, Ida_rose, Ida_item, Ida_event),
+  tell(jeremy, Jeremy_rose, Jeremy_item, Jeremy_event),
+  tell(leroy, Leroy_rose, Leroy_item, Leroy_event),
+  tell(stella, Stella_rose, Stella_item, Stella_event).
 
 all_different([H | T]) :- member(H,T), !, fail.
 all_different([_ | T]) :- all_different(T).
--- schools_out.pl
+++ schools_out.pl
@@ -25,91 +25,83 @@ name(mcEvoy).
 name(parnell).
 
 
-solve:-
-  subject(appleton_subject),
-  subject(gross_subject),
-  subject(knight_subject),
-  subject(mcEvoy_subject),
-  subject(parnell_subject),
-  all_different([appleton_subject, gross_subject, knight_subject, mcEvoy_subject, parnell_subject]),
+solve :-
 
-  state(appleton_state),
-  state(gross_state),
-  state(knight_state),
-  state(mcEvoy_state),
-  state(parnell_state),
-  all_different([appleton_state, gross_state, knight_state, mcEvoy_state, parnell_state]),
+  subject(Appleton_subject),
+  subject(Gross_subject),
+  subject(Knight_subject),
+  subject(Mcevoy_subject),
+  subject(Parnell_subject),
+  all_different([Appleton_subject, Gross_subject, Knight_subject, Mcevoy_subject, Parnell_subject]),
 
-  activity(appleton_activity),
-  activity(gross_activity),
-  activity(knight_activity),
-  activity(mcEvoy_activity),
-  activity(parnell_activity),
-  all_different([appleton_activity, gross_activity, knight_activity, mcEvoy_activity, parnell_activity]),
+  state(Appleton_state),
+  state(Gross_state),
+  state(Knight_state),
+  state(Mcevoy_state),
+  state(Parnell_state),
+  all_different([Appleton_state, Gross_state, Knight_state, Mcevoy_state, Parnell_state]),
 
-  groupings = [ [appleton, appleton_subject, appleton_state, appleton_activity],
-            [gross, gross_subject, gross_state, gross_activity],
-            [knight, knight_subject, knight_state, knight_activity],
-            [mcEvoy, mcEvoy_subject, mcEvoy_state, mcEvoy_activity],
-            [parnell, parnell_subject, parnell_state, parnell_activity] ],
+  activity(Appleton_activity), activity(Gross_activity), activity(Knight_activity), activity(Mcevoy_activity), activity(Parnell_activity),
+  all_different([Appleton_activity, Gross_activity, Knight_activity, Mcevoy_activity, Parnell_activity]),
 
+  Groupings = [ [appleton, Appleton_subject, Appleton_state, Appleton_activity],
+            [gross, Gross_subject, Gross_state, Gross_activity],
+            [knight, Knight_subject, Knight_state, Knight_activity],
+            [mcevoy, Mcevoy_subject, Mcevoy_state, Mcevoy_activity],
+            [parnell, Parnell_subject, Parnell_state, Parnell_activity] ],
 
 %Clue One
-    %gross_subject = math || gross_subject = science
-    %if gross_activity = antiquing -> gross_state = florida
-      %else gross_state = california
+    %Gross_subject = math || Gross_subject = science
+    %if Gross_activity = antiquing -> Gross_state = florida
+      %else Gross_state = california
 
     %member(name, subject, state, activity)
 
-    ( member([gross, math, _, _], groupings) ;
-    member([gross, science, _, _], groupings) ),
+    ( member([gross, math, _, _], Groupings) ; member([gross, science, _, _], Groupings) ),
 
-    ( member([gross, _, florida, antiquing], groupings) ;
-    member([gross, _, california, _], groupings) ),
+    ( member([gross, _, florida, antiquing], Groupings) ; member([gross, _, california, _], Groupings) ),
 
 
 %Clue Two
     %person_subject = science && person_activity = water_skiing
       %person_state = florida || person_state = california
-    %mcEvoy_subject = history && mcEvoy_state = maine || mcEvoy_state = oregon
+    %Mcevoy_subject = history && Mcevoy_state = maine || Mcevoy_state = oregon
 
-    ( member([_, science, florida, water_skiing], groupings) ;
-    member([_, science, california, water_skiing], groupings) ),
+    ( member([_, science, florida, water_skiing], Groupings) ; member([_, science, california, water_skiing], Groupings) ),
 
-    ( member([mcEvoy, history, maine, _], groupings) ;
-    member([mcEvoy, history, oregon, _], groupings) ),
+    ( member([mcevoy, history, maine, _], Groupings) ; member([mcevoy, history, oregon, _], Groupings) ),
 
 
 %Clue Three
-    %(appleton_state = virginia && appleton_subject = english) || parnell_state = virginia
-    %parnell_activity = spelunking
+    %(Appleton_state = virginia && Appleton_subject = english) || Parnell_state = virginia
+    %Parnell_activity = spelunking
 
-    ( member ([appleton, english, virginia, _], groupings) ; member ([parnell, _, virginia, _,], groupings) ),
-    member ([parnell, _, _, spelunking], groupings),
+    ( member([appleton, english, virginia, _], Groupings) ; member([parnell, _, virginia, _], Groupings) ),
+    member([parnell, _, _, spelunking], Groupings),
 
 %Clue Four
     %person_state = maine && person_subject != gym && person_activity != sightseeing
 
-    \+ member ([_, _, maine, sightseeing], groupings),
-    \+ member ([_, gym, main, _], groupings),
+    \+ member([_, _, maine, sightseeing], Groupings),
+    \+ member([_, gym, maine, _], Groupings),
 
 %Clue Five
-    %gross_activity != camping
+    %Gross_activity != camping
     %woman_activity = antiquing
 
-    %member ([gross, _, _, _], groupings), \+ member ([_, _, _, camping], groupings),
-    \+ member ([gross, _, _, camping], groupings),
-    (   member ([gross, _, _, antiquing], groupings) ;
-        member ([appleton, _, _, antiquing], groupings) ;
-        member ([parnell, _, _, antiquing], groupings) ),
+    %member([gross, _, _, _], Groupings), \+ member([_, _, _, camping], Groupings),
 
-    tell(appleton, appleton_subject, appleton_state, appleton_activity),
-    tell(gross, gross_subject, gross_state, gross_activity),
-    tell(knight, knight_subject, knight_state, knight_activity),
-    tell(mcEvoy, mcEvoy_subject, mcEvoy_state, mcEvoy_activity),
-    tell(parnell, parnell_subject, parnell_state, parnell_activity).
+    \+ member([gross, _, _, camping], Groupings),
+    (   member([gross, _, _, antiquing], Groupings) ;
+        member([appleton, _, _, antiquing], Groupings) ;
+        member([parnell, _, _, antiquing], groupings) ),
 
-%Check Logic
+    tell(appleton, Appleton_subject, Appleton_state, Appleton_activity),
+    tell(gross, Gross_subject, Gross_state, Gross_activity),
+    tell(knight, Knight_subject, Knight_state, Knight_activity),
+    tell(mcevoy, Mcevoy_subject, Mcevoy_state, Mcevoy_activity),
+    tell(parnell, Parnell_subject, Parnell_state, Parnell_activity).
+
 all_different([H | T]) :- member(H,T), !, fail.
 all_different([_ | T]) :- all_different(T).
 all_different([_]).
@@ -117,5 +109,5 @@ all_different([_]).
 tell(N, SUB, ST, A) :-
     write(N), write(' who teaches '),
     write(SUB), write(' is vacationing to '),
-    write(ST), write(' and their activity is'),
+    write(ST), write(' and their activity is '),
     write(A), nl.
